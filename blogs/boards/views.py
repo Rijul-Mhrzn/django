02295dfad2060dedacfb9a3389c4bd88e6d3e47c
Rijul-Mhrzn <rijulmaharjan@gmail.com>
2024-04-09 11:58:57 +0545

    Project_updated

--- blogs/boards/views.py
+++ blogs/boards/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 def home(request):
     boards = Board.objects.all()
@@ -16,22 +17,27 @@ def home(request):
     return render(request, 'home.html', {'boards': boards})
 
 def board_topics(request, pk):
+    # return HttpResponse("List of board topics")
     try:
         board = Board.objects.get(pk=pk)
     except Board.DoesNotExist:
         raise Http404
-    return render(request, 'topics.html', {'board':board})
-    # return HttpResponse("List of board topics")
+    return render(request, 'topics.html', {'board': board})
+
+
+
 
 def about(request):
     # do something...
     return render(request, 'about.html')
+    
 
 def question(request, pk):
     return HttpResponse(f"Question : {pk}")
 
+
 def post(request, slug):
-    return HttpResponse(f"slug : {slug}")
+    return HttpResponse(f"Slug : {slug}")
 
 def blog_post(request, slug, pk):
     return HttpResponse(f"Blog_post : {slug} and PK : {pk}")
@@ -40,28 +46,24 @@ def user_profile(request, username):
     return HttpResponse(f"User Name : {username}")
 
 def year_archive(request, year):
-    return HttpResponse(f"Year : {year}")
+    return HttpResponse(f"Year: {year}")
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-
-    if request.method =='POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
-
-        user = User.objects.first() # TODO: get the currently logged in user
-
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
-
-        post = Post.objects.create(
-            message=message,
-            topics=topic,
-            created_by=user
-        )
-        
-        return redirect('board_topics', pk=board.pk) # TODO: redirect to the created topic page
-    return render(request, 'new_topic.html', {'board': board})
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topics=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
